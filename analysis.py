b4fea0272f96aa6fab703f1cef45bef0b55ab09c
Çağıl Uluşahin <cagilulusahin@gmail.com>
2014-07-16 01:16:25 +0300

    no extra spellcheck

--- analysis.py
+++ analysis.py
@@ -383,8 +383,8 @@ def show_results(res_mat,mapp, not_ovv = [], max_val = [1., 1., 0.5, 0.0, 1.0, 0
             res_list = []
             if res_dict:
                 for res_ind,cand in enumerate(res_dict):
-                    if(not tools.spell_check(cand)): #or ovv == cand): # spell check on graph is problematic
-                        continue;
+                    #if(not tools.spell_check(cand)): #or ovv == cand): # spell check on graph is problematic
+                    #    continue;
                     score = calculate_score(res_dict[cand],max_val)
                     if score >= threshold:
                         res_dict[cand].append(round(score,7))
